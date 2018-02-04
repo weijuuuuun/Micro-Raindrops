3eecc6b76c0843f0465aa25827976259267d1e8b
Wei Chai <wei4.chai@live.uwe.ac.uk>
2018-02-04 22:55:20 +0000

    challenge1

---
+++ Challenge 1/Raindrop/source/main.cpp
@@ -0,0 +1,76 @@
+#include "MicroBit.h"
+
+MicroBit uBit;
+
+// set raindrop position
+int rainX = 2;
+int rainY = 0;
+
+// set tray position
+int tray = 2;
+
+int score = 0;
+
+void onButtonA(MicroBitEvent e) {
+  tray--;
+  if (tray == 0) {
+    tray = 0;
+  }
+}
+
+void onButtonB(MicroBitEvent e) {
+  tray++;
+  if (tray == 4) {
+    tray = 4;
+  }
+}
+
+bool isCaught(){
+  // checks if raindrop & tray is in same position
+  if (rainX = tray && rainY = 4) {
+    resetRain();
+    return true;
+  }
+}
+
+void resetRain(){
+  rainX = uBit.random(5);
+  rainY = -1;
+}
+
+int main(){
+  // plot raindrop
+  uBit.display.image.setPixelValue(rainX,rainY,255);
+
+  // plot tray
+  uBit.display.image.setPixelValue(tray,4,255);
+
+  uBit.init();
+
+
+  while(1){
+      // unplot raindrop & tray
+      uBit.display.image.clear();
+
+      // plot rain position y++ (dropping)
+      rainY++;
+      uBit.display.image.setPixelValue(rainX,rainY,255);
+
+      // set up listener for buttonA and buttonB
+      uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+      uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
+
+      // plot tray position
+      uBit.display.image.setPixelValue(tray,4,255);
+
+      if (isCaught()) {
+        score++;
+      } else if (rainY > 4) {
+        // game over
+        uBit.display.print("X")
+        // display score
+        uBit.display.scroll(score);
+      }
+      ubit.sleep(500);
+  }
+}
